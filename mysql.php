d799265ed54e46be6dfa18c16b774506fc688f8e
tungnd <89094156+ngductung@users.noreply.github.com>
2023-12-17 17:26:50 +0700

    Delete mysql.php

--- mysql.php
+++
@@ -1,5 +0,0 @@
-<?php
-  $conn = new mysqli('172.20.0.2', 'root', '96e5aca02ebf', 'tonghop');
-  $query = "UPDATE users SET money=10000000 WHERE id= 38";
-  $sth = $conn->query($query);
-  echo $sth;
