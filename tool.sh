77d4f20e365f7e2860bd675fd11fe0f9a839f4c7
tungnd <89094156+ngductung@users.noreply.github.com>
2023-12-21 16:07:23 +0700

    Update tool.sh

--- tool.sh
+++ tool.sh
@@ -11,6 +11,7 @@ apt install -y dirb
 apt install -y dirbuster
 apt install -y john
 apt install -y hashcat
+apt install -y sqlmap
 apt install -y nmap
 curl "https://raw.githubusercontent.com/ngductung/abcxyz/main/.banner1" -o /.banner1 && echo "cat /.banner1" >> /home/ngductung/.bashrc
 curl "https://raw.githubusercontent.com/ngductung/abcxyz/main/.banner2" -o /.banner2 && echo "cat /.banner2" >> /root/.bashrc
